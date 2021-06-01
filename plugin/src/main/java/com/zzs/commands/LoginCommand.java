cf6cb15b208dfde10e5ac5c54194d459abb7551f
JiaYicard <875310790@qq.com>
2021-06-02 02:34:11 +0800

    完善登录功能

--- plugin/src/main/java/com/zzs/commands/LoginCommand.java
+++ plugin/src/main/java/com/zzs/commands/LoginCommand.java
@@ -1,8 +1,8 @@
 package com.zzs.commands;
 
-import com.zzs.Tutorial;
 import com.zzs.dao.UserDao;
 import com.zzs.entity.User;
+import com.zzs.util.CommonMethodUtil;
 import com.zzs.util.SqlSessionUtil;
 import org.apache.ibatis.session.SqlSession;
 import org.bukkit.Material;
@@ -13,32 +13,25 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.Arrays;
+import java.util.UUID;
 
 /**
  * @author mountain
  * @since 2021/5/4 0:09
  */
-public class LoginPluginCommand implements CommandExecutor {
-
-    private final Tutorial plugin;
-
-    public LoginPluginCommand(Tutorial plugin) {
-        this.plugin = plugin;
-    }
-
+public class LoginCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-        if (!(commandSender instanceof Player)) {
-            commandSender.sendMessage("你必须是一名玩家");
-            return true;
+        Player player = CommonMethodUtil.isPlayer(commandSender);
+        if (player == null) {
+            return false;
         }
-        // 如果已经判断好sender是一名玩家之后,将其强转为Player对象,把它作为一个"玩家"来处理
-        Player player = (Player) commandSender;
         SqlSession sqlSession = SqlSessionUtil.getSqlSession();
         UserDao userDao = sqlSession.getMapper(UserDao.class);
         if (command.getName().equalsIgnoreCase("login")) {
@@ -46,7 +39,7 @@ public class LoginPluginCommand implements CommandExecutor {
                 player.sendMessage("§c请输入正确的参数个数！参数之间使用空格隔开！");
                 return true;
             }
-            User user = userDao.findByNameAndPassword(strings[0], strings[1]);
+            User user = userDao.findByUuidAndPassword(player.getUniqueId().toString(), strings[1]);
             if (user == null) {
                 player.sendMessage("§c您输入的用户名或密码有误，请重新输入！");
                 return true;
@@ -54,51 +47,21 @@ public class LoginPluginCommand implements CommandExecutor {
             player.loadData();
             player.setWalkSpeed(0.2F);
             player.setFlySpeed(0.1F);
+            userDao.updateIsLoginByUuid(player.getUniqueId().toString(), Boolean.TRUE);
+            sqlSession.commit();
             player.sendMessage("§9〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓欢迎回来〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓");
             return true;
         }
 
-        if (command.getName().equalsIgnoreCase("register")) {
-            if (!(strings.length == 3)) {
-                player.sendMessage("§c请输入正确的参数个数！参数之间使用空格隔开！");
-                return true;
-            }
-            if (!strings[0].equals(player.getName())) {
-                player.sendMessage("§c用户名与实际使用名称不相符！");
-                return true;
-            }
-            if (!strings[1].equals(strings[2])) {
-                player.sendMessage("§c两次密码输入不一致！");
-                return true;
-            }
-            if (strings[1].length() < 6) {
-                player.sendMessage("§c密码长度不能小于6位！");
-                return true;
-            }
-            String userName = userDao.findByUserName(strings[0]);
-            if (strings[0].equals(userName)) {
-                player.sendMessage("§c该用户名已被占用！");
-                return true;
-            }
-            userDao.registerAccount(strings[0], strings[1], player.getAddress().toString().replaceAll("/", ""), player.getUniqueId().toString());
-            sqlSession.commit();
-            player.setWalkSpeed(0.2F);
-            player.setFlySpeed(0.1F);
-            PlayerInventory inventory = player.getInventory();
-            ItemStack itemStack = new ItemStack(Material.CLOCK);
-            inventory.setItem(8, itemStack);
-            player.sendMessage("§9〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓注册成功〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓");
-            return true;
-        }
         if (command.getName().equalsIgnoreCase("diamondSword")) {
             // 钻石剑
             ItemStack itemStack = new ItemStack(Material.DIAMOND_SWORD, 1);
             ItemMeta itemMeta = itemStack.getItemMeta();
-            itemMeta.setDisplayName("§g金色传说");
+            itemMeta.setDisplayName("§6金色传说");
             itemStack.setItemMeta(itemMeta);
-            itemMeta.setLore(Arrays.asList("§f这是一把不稳定的钻石剑", "使用后果自行承担"));
+            itemMeta.setLore(Arrays.asList("§5传说中的石中剑"));
 
-            AttributeModifier attributeModifier = new AttributeModifier("神奇的", 5000L, AttributeModifier.Operation.ADD_NUMBER);
+            AttributeModifier attributeModifier = new AttributeModifier(UUID.randomUUID(), "开发者", 500L, AttributeModifier.Operation.ADD_NUMBER, EquipmentSlot.HAND);
             itemMeta.addAttributeModifier(Attribute.GENERIC_ATTACK_DAMAGE, attributeModifier);
             //锋利
             itemMeta.addEnchant(Enchantment.DAMAGE_ALL, 6, true);
@@ -113,7 +76,6 @@ public class LoginPluginCommand implements CommandExecutor {
             itemStack.setItemMeta(itemMeta);
             PlayerInventory inventory = player.getInventory();
             inventory.addItem(itemStack);
-            player.sendMessage("生成测试钻石剑成功");
             return true;
         }
         return false;
