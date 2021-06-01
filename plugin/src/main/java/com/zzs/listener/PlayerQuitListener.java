6dadd744a6dcdffbbacd55018c8c2346de001788
JiaYicard <875310790@qq.com>
2021-06-02 02:36:30 +0800

    玩家退出事件

--- plugin/src/main/java/com/zzs/listener/PlayerQuitListener.java
+++ plugin/src/main/java/com/zzs/listener/PlayerQuitListener.java
@@ -1,5 +1,8 @@
 package com.zzs.listener;
 
+import com.zzs.dao.UserDao;
+import com.zzs.util.SqlSessionUtil;
+import org.apache.ibatis.session.SqlSession;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,9 +13,18 @@ import org.bukkit.event.player.PlayerQuitEvent;
  * @since 2021/5/31 20:50
  */
 public class PlayerQuitListener implements Listener {
+    /**
+     * 玩家退出事件
+     *
+     * @param event
+     */
     @EventHandler
     public void onQuit(PlayerQuitEvent event) {
         Player player = event.getPlayer();
         player.saveData();
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        UserDao userDao = sqlSession.getMapper(UserDao.class);
+        userDao.updateIsLoginByUuid(player.getUniqueId().toString(), Boolean.FALSE);
+        sqlSession.commit();
     }
 }
