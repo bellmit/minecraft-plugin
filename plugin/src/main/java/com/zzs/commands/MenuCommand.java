57ba8f44869fedd4f658fab4c37acb72590647af
JiaYicard <875310790@qq.com>
2021-06-02 02:35:11 +0800

    菜单功能

---
+++ plugin/src/main/java/com/zzs/commands/MenuCommand.java
@@ -0,0 +1,28 @@
+package com.zzs.commands;
+
+import com.zzs.util.CommonMethodUtil;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+
+/**
+ * @author mountain
+ * @since 2021/6/1 23:50
+ */
+public class MenuCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        Player player = CommonMethodUtil.isPlayer(commandSender);
+        if (player == null) {
+            return false;
+        }
+        if (command.getName().equalsIgnoreCase("menu")) {
+            Inventory inventory = CommonMethodUtil.createMenu();
+            player.openInventory(inventory);
+            return true;
+        }
+        return false;
+    }
+}
--- plugin/src/main/java/com/zzs/listener/InventoryClickListener.java
+++ plugin/src/main/java/com/zzs/listener/InventoryClickListener.java
@@ -1,17 +1,12 @@
 package com.zzs.listener;
 
-import org.bukkit.Bukkit;
+import com.zzs.util.CommonMethodUtil;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author mountain
@@ -19,38 +14,17 @@ import java.util.List;
  */
 public class InventoryClickListener implements Listener {
 
+    /**
+     * 玩家点击事件
+     *
+     * @param event
+     */
     @EventHandler
     public void onClick(InventoryClickEvent event) {
         if (event.getCurrentItem().getType().equals(Material.CLOCK) && event.getClick().isRightClick()) {
             Player player = (Player) event.getWhoClicked();
-            Inventory inventory = Bukkit.createInventory(null, 9, "菜单栏");
-            ItemStack itemStack = new ItemStack(Material.PLAYER_HEAD);
-            ItemMeta itemMeta = itemStack.getItemMeta();
-            itemMeta.setDisplayName("个人信息");
-            List<String> infoLore = new ArrayList<>();
-            infoLore.add("等级：xxx");
-            infoLore.add("金钱：xxx");
-            itemMeta.setLore(infoLore);
-            itemStack.setItemMeta(itemMeta);
-            inventory.addItem(itemStack);
+            Inventory inventory = CommonMethodUtil.createMenu();
             player.openInventory(inventory);
-        }
-    }
-
-    public void onClick1(InventoryClickEvent event) {
-        // 获取被点击的容器的对象
-        Inventory inv = event.getInventory();
-        // 因为Inventory底层的实现是由一个数组来进行管理的, 所以我们的首项就是0
-        if (event.getRawSlot() == 0) {
-            // 获取点击容器的玩家的对象
-            Player player = (Player) event.getWhoClicked();
-            // 对该玩家进行传送的操作
-            player.teleport(player.getLocation().add(1D, 1D, 1D));
-            // 因为我们不想让玩家拿走该物品所以我们使用
-            //  方法来取消该事件的执行
-            event.setCancelled(true);
-            // 让玩家关闭当前的容器界面
-            player.closeInventory();
         }
     }
 }
