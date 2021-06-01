c75382d5b74f26d3cbe1f2b3bbbc8beb47ad8ca5
JiaYicard <875310790@qq.com>
2021-06-02 02:37:10 +0800

    玩家命令事件

--- plugin/src/main/java/com/zzs/listener/PlayerCommandPreprocessListener.java
+++ plugin/src/main/java/com/zzs/listener/PlayerCommandPreprocessListener.java
@@ -1,16 +1,28 @@
 package com.zzs.listener;
 
+import com.zzs.dao.UserDao;
+import com.zzs.util.SqlSessionUtil;
+import org.apache.ibatis.session.SqlSession;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
 /**
- * @author mm013
- * @Date 2021/5/31 16:37
+ * @author mountain
+ * @since 2021/6/2 0:46
  */
-public class PlayerCommandPreprocessListener {
-
+public class PlayerCommandPreprocessListener implements Listener {
 
     @EventHandler
-    public void onCommand(PlayerCommandPreprocessEvent event) {
+    public void onCommandPreprocess(PlayerCommandPreprocessEvent event) {
+        Player player = event.getPlayer();
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        UserDao userDao = sqlSession.getMapper(UserDao.class);
+        Boolean isLogin = userDao.findIsLoginByUuid(player.getUniqueId().toString());
+        if (!isLogin && !event.getMessage().contains("/login") && !event.getMessage().contains("/register")) {
+            player.sendMessage("§c您还未登录！无权使用该命令！");
+            event.setMessage("/o");
+        }
     }
 }
