259e600ee4dbc76872f4f7b676828a6e16afc354
Mountain <15627991561@139.com>
2021-06-02 17:54:40 +0800

    修复未登录情况下退出物品丢失

--- plugin/src/main/java/com/zzs/listener/PlayerQuitListener.java
+++ plugin/src/main/java/com/zzs/listener/PlayerQuitListener.java
@@ -21,9 +21,13 @@ public class PlayerQuitListener implements Listener {
     @EventHandler
     public void onQuit(PlayerQuitEvent event) {
         Player player = event.getPlayer();
-        player.saveData();
         SqlSession sqlSession = SqlSessionUtil.getSqlSession();
         UserDao userDao = sqlSession.getMapper(UserDao.class);
+        Boolean isLogin = userDao.findIsLoginByUuid(player.getUniqueId().toString());
+        if (!isLogin) {
+            //如果玩家未登录情况下退出则不保存数据（加入的时候清空了背包防止保存覆盖）
+            player.loadData();
+        }
         userDao.updateIsLoginByUuid(player.getUniqueId().toString(), Boolean.FALSE);
         sqlSession.commit();
     }
