522f016b6f4e14ba2a6a3ce786828a943a112e75
Mountain <15627991561@139.com>
2021-06-09 17:44:52 +0800

    自定义世界规则

--- plugin/src/main/java/com/zzs/commands/WorldCommand.java
+++ plugin/src/main/java/com/zzs/commands/WorldCommand.java
@@ -43,6 +43,19 @@ public class WorldCommand implements CommandExecutor {
             world.setGameRule(GameRule.DO_MOB_SPAWNING, Boolean.FALSE);
             //是否死亡不掉落
             world.setGameRule(GameRule.KEEP_INVENTORY, Boolean.TRUE);
+            //管理员命令记录
+            world.setGameRule(GameRule.LOG_ADMIN_COMMANDS, Boolean.TRUE);
+            //死亡直接重生
+            world.setGameRule(GameRule.DO_IMMEDIATE_RESPAWN, Boolean.TRUE);
+            //流浪商人
+            world.setGameRule(GameRule.DO_TRADER_SPAWNING, Boolean.FALSE);
+            //跌落伤害
+            world.setGameRule(GameRule.FALL_DAMAGE, Boolean.FALSE);
+            //火焰伤害
+            world.setGameRule(GameRule.FIRE_DAMAGE, Boolean.FALSE);
+            //窒息伤害
+            world.setGameRule(GameRule.DROWNING_DAMAGE, Boolean.FALSE);
+            //自动保存
             world.setAutoSave(true);
             player.sendMessage("创建" + world.getName() + "世界成功");
             return true;
@@ -52,15 +65,24 @@ public class WorldCommand implements CommandExecutor {
             List<World> worlds = plugin.getServer().getWorlds();
             player.sendMessage("§b〓〓〓〓〓〓〓〓〓〓〓〓〓〓世界名称列表〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓");
             worlds.forEach(s -> player.sendMessage(s.getName()));
+            return true;
         }
 
-        if (command.getName().equals("teleportWorld")) {
+        if (command.getName().equalsIgnoreCase("warp")) {
             if (!(args.length == 1)) {
                 player.sendMessage("§c请查看正确的命令使用规则！");
+                return false;
             }
-            World world = plugin.getServer().getWorld(args[0]);
-            player.teleport(world.getSpawnLocation());
-            player.sendMessage("成功传送到：" + world.getName());
+            List<World> worlds = plugin.getServer().getWorlds();
+            worlds.forEach(s -> {
+                if (s.equals(args[0])) {
+                    World world = plugin.getServer().getWorld(args[0]);
+                    player.teleport(world.getSpawnLocation());
+                    player.sendMessage("成功传送到：" + world.getName());
+                } else {
+                    player.sendMessage("§c请输入正确的世界名称！");
+                }
+            });
             return true;
         }
         return false;
