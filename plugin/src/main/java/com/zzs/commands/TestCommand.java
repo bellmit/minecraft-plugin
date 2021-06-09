112b11c203a0d08da4f7b1807ea401b8b608f912
Mountain <15627991561@139.com>
2021-06-09 17:44:34 +0800

    生成菜单物品

--- plugin/src/main/java/com/zzs/commands/TestCommand.java
+++ plugin/src/main/java/com/zzs/commands/TestCommand.java
@@ -1,11 +1,16 @@
 package com.zzs.commands;
 
-import org.bukkit.Statistic;
+import com.zzs.util.CommonMethodUtil;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.Arrays;
 
 /**
  * @author mm013
@@ -14,11 +19,19 @@ import org.bukkit.entity.Player;
 public class TestCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        Player player = (Player) sender;
-        if (command.getName().equals("test")) {
-            player.getStatistic(Statistic.PLAYER_KILLS);
-            player.sendMessage("PLAYER_KILLS：" + player.getStatistic(Statistic.PLAYER_KILLS));
-            player.sendMessage("KILL_ENTITY：" + player.getStatistic(Statistic.KILL_ENTITY, EntityType.ZOMBIE));
+        Player player = CommonMethodUtil.isPlayer(sender);
+        if (player == null) {
+            return false;
+        }
+        if (command.getName().equalsIgnoreCase("test")) {
+            PlayerInventory inventory = player.getInventory();
+            ItemStack itemStack = new ItemStack(Material.CLOCK);
+            ItemMeta itemMeta = itemStack.getItemMeta();
+            itemMeta.setDisplayName("菜单");
+            itemMeta.setLore(Arrays.asList("右键打开菜单栏"));
+            itemStack.setItemMeta(itemMeta);
+            inventory.addItem(itemStack);
+            return true;
         }
         return false;
     }
